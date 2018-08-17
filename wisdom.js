76a1109eb679128d510659bb75c9934b8b9ee0b6
Gabriel Morris <gabemorris@outlook.com>
2018-08-16 23:54:04 -0700

    Abstract everything into functions

--- wisdom.js
+++ wisdom.js
@@ -2,18 +2,44 @@
 const quotes = require("./quotes.json");
 
 exports.getQuote = function() {
+  // Get random number between 0-100
   let random = Math.floor(Math.random() * 100);
 
-  if (random < 2) {
-    return `${quotes.animalsOne} are ${quotes.animalsTwo}`;
+  // Small probability of animal based quote
+  if (random < 5) {
+    return generateAnimalQuote(quotes);
   } else {
-    let randomOne = Math.floor(Math.random() * quotes.partOne.length);
-    let randomTwo = Math.floor(Math.random() * quotes.partTwo.length);
-    let randomThree = Math.floor(Math.random() * quotes.partThree.length);
-    let randomFour = Math.floor(Math.random() * quotes.partFour.length);
+    // Get four random numbers for the four random parts of a quote
+    let randomNumberArray = generateRandomNumberArray();
 
-    return `${quotes.partOne[randomOne]} ${quotes.partTwo[randomTwo]} ${
-      quotes.partThree[randomThree]
-    } ${quotes.partFour[randomFour]}`;
+    // Return quote string
+    return generateQuoteString(quotes, randomNumberArray);
+  }
+
+  // Function for getting random number based on part length
+  function getRandom(part) {
+    return Math.floor(Math.Random * part.length);
+  }
+
+  // Function for generating quote string
+  function generateQuoteString(quotes, randArr) {
+    return `${quotes.partOne[randArr[0]]} ${quotes.partTwo[randArr[1]]} ${
+      quotes.partThree[randArr[2]]
+    } ${quotes.partFour[randArr[3]]}`;
+  }
+
+  // Function for generating animal quote string
+  function generateAnimalQuote(quotes) {
+    return `${quotes.animalsOne} are ${quotes.animalsTwo}`;
+  }
+
+  // Function for generating random numbers
+  function generateRandomNumberArray() {
+    return [
+      getRandom(quotes.partOne.length),
+      getRandom(quotes.partTwo.length),
+      getRandom(quotes.partThree.length),
+      getRandom(quotes.partFour.length)
+    ];
   }
 };
