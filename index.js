0d8b468e47d4905e6de82fce4c4edb23de16bf23
Gabriel Morris <gabemorris@outlook.com>
2018-08-16 21:40:58 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+const quotes = require("./quotes.json");
+
+exports.getQuotes = function() {
+  let random = Math.floor(Math.random() * 100);
+
+  if (random < 2) {
+    return `${quotes.animalsOne} are ${quotes.animalsTwo}`;
+  } else {
+    let randomOne = Math.floor(Math.random() * quotes.partOne.length);
+    let randomTwo = Math.floor(Math.random() * quotes.partTwo.length);
+    let randomThree = Math.floor(Math.random() * quotes.partThree.length);
+    let randomFour = Math.floor(Math.random() * quotes.partFour.length);
+
+    return `${quotes.partOne[randomOne]} ${quotes.partTwo[randomTwo]} ${
+      quotes.partThree[randomThree]
+    } ${quotes.partFour[randomFour]}`;
+  }
+};
